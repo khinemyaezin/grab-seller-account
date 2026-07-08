60d3654b117fa72b4a7dbc51ff381fd31b392bbe
khinemyaezin <khinemyaezin@icloud.com>
2026-07-08 20:20:47 +0700

    refactor: migrate to Tailwind CSS Vite plugin and scope styles to MFE container

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { federation } from '@module-federation/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 import { fileURLToPath, URL } from "node:url";
+import tailwindcss from '@tailwindcss/vite';
+//import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: env.NODE_ENV === 'production' ? '/mfe/seller-account/' : '/',
+    base: env.NODE_ENV === 'development' ? 'http://localhost:3004/' : '/mfe/seller-account/',
     plugins: [
       react(),
+      tailwindcss(),
+      // cssInjectedByJsPlugin(),
       federation({
         name: 'grab_seller_account',
         filename: 'remoteEntry.js',
